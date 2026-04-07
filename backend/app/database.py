f7c8613cf632ba0004305beede8cb13d13776d98
1agoRP <iagopradoiago@hotmail.com>
2026-04-07 10:30:27 -0300

    Fix: Definitive solution for PgBouncer DuplicatePreparedStatementError using anonymous statements

--- backend/app/database.py
+++ backend/app/database.py
@@ -23,6 +23,7 @@ engine = create_async_engine(
     connect_args={
         "statement_cache_size": 0,
         "prepared_statement_cache_size": 0,
+        "prepared_statement_name_callback": lambda: "",
     },
 )
 
